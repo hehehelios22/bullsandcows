74e7f699f147b7b3d0bd8ba07f69b53a827b6faa
hehehelmans <hehehelios@gmail.com>
2019-12-12 22:28:05 +0100

    no message

--- Solution/BullsAndCows/BullsAndCows.cpp
+++ Solution/BullsAndCows/BullsAndCows.cpp
@@ -1,13 +1,47 @@
 ﻿#include <iostream>
+#include <string>
 
 using namespace std;
 
+void PrintIntro();
+string GetGuessAndPrint();
+// the entry point of our application
+
+
 int main()
 {
+	
+	PrintIntro();
+	
+	GetGuessAndPrint();
+	GetGuessAndPrint();
+
+	cout << endl;
+	return 0;
+}
+
+
+
+string GetGuessAndPrint() {
+	// get a guess from the player 
+	cout << "Enter your guess: ";
+	string Guess = "";
+	getline(cin, Guess);
+	// repeat the guess back to them
+	cout << "Your guess was: " << Guess << endl;
+	return Guess;
+}
+
+
+void PrintIntro() {
+
+	// introduce the game
+
+
 	constexpr int WORD_LENGTH = 9;
 	cout << "Eluwina. A fn game for the whole family: Bulls and Cows. The word game." << endl;
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of? :D\n";
-
-	return 0;
+	cout << endl;
+	return;
 }
