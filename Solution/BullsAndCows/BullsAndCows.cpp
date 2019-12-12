e3311c0f87c306b06a437c9c813d8554f4c119f8
hehehelmans <hehehelios@gmail.com>
2019-12-12 23:39:21 +0100

    Play again? Yes /No

--- Solution/BullsAndCows/BullsAndCows.cpp
+++ Solution/BullsAndCows/BullsAndCows.cpp
@@ -4,18 +4,23 @@
 using namespace std;
 
 void PrintIntro();
-string GetGuess();
-string PrintBack();
 void PlayGame();
+string GetGuess();
+bool AskToPlayAgain();
 // the entry point of our application
 
 
 int main()
 {
-	
-	PrintIntro();
-	PlayGame();
-	return 0;
+	bool bPlayAgain = false; 
+	do {
+		PrintIntro();
+		PlayGame();
+		bPlayAgain = AskToPlayAgain();
+
+	}
+	while (bPlayAgain);
+	return 0; // exit the application
 }
 
 
@@ -45,15 +50,27 @@ string GetGuess()
 
 
 
+
 void PrintIntro() {
 
 	// introduce the game
 
 
 	constexpr int WORD_LENGTH = 9;
-	cout << "Eluwina. A fn game for the whole family: Bulls and Cows. The word game." << endl;
+	cout << "Eluwina. A fun game for the whole family: Bulls and Cows. The word game." << endl;
 	cout << "Can you guess the " << WORD_LENGTH;
 	cout << " letter isogram I'm thinking of? :D\n";
 	cout << endl;
 	return;
+}
+
+
+
+bool AskToPlayAgain()
+{
+
+	cout << "Do you want to play again? ";
+	string Response = "";
+	getline(cin, Response);
+	return (Response[0] == 'y') || (Response[0] == 'Y');
 }
