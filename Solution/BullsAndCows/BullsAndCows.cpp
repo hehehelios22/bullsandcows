b10295c7aa9a2149576244d427c59e070a6661da
hehehelmans <hehehelios@gmail.com>
2019-12-12 21:14:36 +0100

    Magic numbers and constants

--- Solution/BullsAndCows/BullsAndCows.cpp
+++ Solution/BullsAndCows/BullsAndCows.cpp
@@ -1,4 +1,13 @@
-﻿int main()
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
 {
+	constexpr int WORD_LENGTH = 9;
+	cout << "Eluwina. A fn game for the whole family: Bulls and Cows. The word game." << endl;
+	cout << "Can you guess the " << WORD_LENGTH;
+	cout << " letter isogram I'm thinking of? :D\n";
+
 	return 0;
 }
