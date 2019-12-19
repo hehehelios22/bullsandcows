fe32f42f1bac038f46fc20f9aa468db18db40b6a
hehehelmans <hehehelios@gmail.com>
2019-12-19 14:34:43 +0100

    FINAL 8D It works :D

--- Solution/BullsAndCows/FBullCowGame.cpp
+++ Solution/BullsAndCows/FBullCowGame.cpp
@@ -1,25 +1,116 @@
+#pragma once
+
 #include "FBullCowGame.h"
+#include <map>
+
+// to make syntax Unreal friendly
+#define TMap std::map
+using int32 = int;
+
+FBullCowGame::FBullCowGame() { Reset(); } // default constructor
+
+int32 FBullCowGame::GetCurrentTry() const { return MyCurrentTry; }
+int32 FBullCowGame::GetHiddenWordLength() const { return MyHiddenWord.length(); }
+bool FBullCowGame::IsGameWon() const { return bGameIsWon; }
+
+int32 FBullCowGame::GetMaxTries() const
+{
+	TMap<int32, int32> WordLengthToMaxTries{ {3,4}, {4,7}, {5,10}, {6,16}, {7,20} };
+	return WordLengthToMaxTries[MyHiddenWord.length()];
+}
 
 void FBullCowGame::Reset()
 {
+	const FString HIDDEN_WORD = "plane"; // this MUST be an isogram
+	MyHiddenWord = HIDDEN_WORD;
+
+	MyCurrentTry = 1;
+	bGameIsWon = false;
+	return;
 }
 
-int FBullCowGame::GetMaxTries()
+
+EGuessStatus FBullCowGame::CheckGuessValidity(FString Guess) const
 {
-	return 0;
+	if (!IsIsogram(Guess)) // if the guess isn't an isogram
+	{
+		return EGuessStatus::Not_Isogram;
+	}
+	else if (!IsLowercase(Guess)) // if the guess isn't all lowercase
+	{
+		return EGuessStatus::Not_Lowercase;
+	}
+	else if (Guess.length() != GetHiddenWordLength()) // if the guess length is wrong
+	{
+		return EGuessStatus::Wrong_Length;
+	}
+	else
+	{
+		return EGuessStatus::OK;
+	}
 }
 
-int FBullCowGame::GetCurrentTry()
+
+// receives a VALID guess, incriments turn, and returns count
+FBullCowCount FBullCowGame::SubmitValidGuess(FString Guess)
 {
-	return 0;
+	MyCurrentTry++;
+	FBullCowCount BullCowCount;
+	int32 WordLength = MyHiddenWord.length(); // assuming same length as guess
+
+	// loop through all letters in the hidden word
+	for (int32 MHWChar = 0; MHWChar < WordLength; MHWChar++) {
+		// compare letters against the guess
+		for (int32 GChar = 0; GChar < WordLength; GChar++) {
+			// if they match then
+			if (Guess[GChar] == MyHiddenWord[MHWChar]) {
+				if (MHWChar == GChar) { // if they're in the same place
+					BullCowCount.Bulls++; // incriment bulls
+				}
+				else {
+					BullCowCount.Cows++; // must be a cow
+				}
+			}
+		}
+	}
+	if (BullCowCount.Bulls == WordLength) {
+		bGameIsWon = true;
+	}
+	else
+	{
+		bGameIsWon = false;
+	}
+	return BullCowCount;
 }
 
-bool FBullCowGame::IsGameWon()
+bool FBullCowGame::IsIsogram(FString Word) const
 {
-	return false;
+	// treat 0 and 1 letter words as isograms
+	if (Word.length() <= 1) { return true; }
+
+	TMap<char, bool> LetterSeen; // setup our map
+	for (auto Letter : Word) 	// for all letters of the word
+	{
+		Letter = tolower(Letter); // handle mixed case
+		if (LetterSeen[Letter]) {// if the letter is in the map
+			return false; // we do NOT have an isogram
+		}
+		else {
+			LetterSeen[Letter] = true;// add the letter to the map
+		}
+	}
+
+	return true; // for example in cases where /0 is entered
 }
 
-bool FBullCowGame::CheckGuessValidity(std::string)
+bool FBullCowGame::IsLowercase(FString Word) const
 {
-	return false;
+	for (auto Letter : Word)
+	{
+		if (!islower(Letter)) // if not a lowercase letter
+		{
+			return false;
+		}
+	}
+	return true;
 }
--- Solution/BullsAndCows/FBullCowGame.h
+++ Solution/BullsAndCows/FBullCowGame.h
@@ -1,19 +1,52 @@
+/* The game logic (no view code or direct user interaction)
+The game is a simple guess the word game based on Mastermind :)
+*/
+
 #pragma once
 #include <string>
+
+// to make syntax Unreal friendly
+using FString = std::string;
+using int32 = int;
+
+struct FBullCowCount
+{
+	int32 Bulls = 0;
+	int32 Cows = 0;
+};
+
+enum class EGuessStatus
+{
+	Invalid_Status,
+	OK,
+	Not_Isogram,
+	Wrong_Length,
+	Not_Lowercase
+};
+
 
 class FBullCowGame
 {
 public:
-	void Reset(); //TODO make a more rich return value.
-	int GetMaxTries();
-	int GetCurrentTry();
-	bool IsGameWon();
-	bool CheckGuessValidity(std::string); // TODO make a more rich return value
+	FBullCowGame(); // constructor
 
-// Please ignore this shit and look up 
+	int32 GetMaxTries() const;
+	int32 GetCurrentTry() const;
+	int32 GetHiddenWordLength() const;
+	bool IsGameWon() const;
+	EGuessStatus CheckGuessValidity(FString) const;
+
+	void Reset();
+	FBullCowCount SubmitValidGuess(FString);
+
+
+	// ^^ Please try and ignore this and focus on the interface above UwU ^^
 private:
-	int MyCurrentTry;
-	int MyMaxTries;
-	
+	// see constructor for initialisation
+	int32 MyCurrentTry;
+	FString MyHiddenWord;
+	bool bGameIsWon;
 
+	bool IsIsogram(FString) const;
+	bool IsLowercase(FString) const;
 };
